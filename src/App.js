a1c29c1f7ccc4d3645a2eabafa2499a8c447085e
FedoseevDS <dmsr.fedoseev@gmail.com>
2024-01-26 23:48:36 +0300

    добавил react-router-dom and sass

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Main } from './pages/main/main';
+import './styles/global.scss';
 
 function App() {
   return (
-    <div>Работает!!!</div>
+    <Routes>
+      <Route path='/' element={<Main />} />
+    </Routes>
   );
 }
 
